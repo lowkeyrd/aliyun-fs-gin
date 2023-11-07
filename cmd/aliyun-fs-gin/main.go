381227a896411857f1619f90b223afcbc0d21284
s.y.s <s.y.s.sam.sys@gmail.com>
2023-11-07 15:42:10 +0800

    update

--- cmd/aliyun-fs-gin/main.go
+++ cmd/aliyun-fs-gin/main.go
@@ -2,49 +2,32 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
-
-// Defining the Graphql handler
-/*
-func graphqlHandler(logger *zap.Logger) echo.HandlerFunc {
-	srv := handler.NewDefaultServer()
-	return func(c echo.Context) error {
-		srv.ServeHTTP(c.Response(), c.Request())
-		return nil
-	}
-}
-*/
-
-func playgroundHandler() echo.HandlerFunc {
-	h := playground.Handler("GraphQL playground", "/")
-	return func(c echo.Context) error {
-		h.ServeHTTP(c.Response(), c.Request())
-		return nil
-	}
-}
 
 func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	r := echo.New()
-
-	r.GET("/", playgroundHandler())
-	//r.POST("/", graphqlHandler(client, logger))
-	//r.POST("/", graphqlHandler(logger))
+	e := echo.New()
+	e.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/ping")
+	})
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "hello world")
+	})
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 	// Start server
 	go func() {
-		if err := r.Start(":8080"); err != nil {
-			r.Logger.Info("shutting down the server")
+		if err := e.Start(":8080"); err != nil {
+			e.Logger.Info("shutting down the server")
 		}
 	}()
 
@@ -55,7 +38,7 @@ func main() {
 	<-quit
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if err := r.Shutdown(ctx); err != nil {
-		r.Logger.Fatal(err)
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
 	}
 }
